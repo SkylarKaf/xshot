123052e66a4b600f31aacd2ee2af7ff6beeabd7a
Az <azry695@gmail.com>
2023-01-25 18:21:13 +0700

    Wm

--- xshot.sh
+++ xshot.sh
@@ -51,8 +51,8 @@ border_c_light="${hex_color[2]}"
 shadow_size="85x10+0+10"
 shadow_color="${hex_color[3]}"
 # footer text
-owner_info=" @Krisna"
-footer_text=" Shot by NevtGanz"
+owner_info=" @AzRyCb"
+footer_text=" Shot by AzRyCb"
 #footer style
 #xshot
 footer_xy="+0+30"
